92e39858b8cc358779b2f926d614cb5034c53166
Alikeevich <alikkenzheb@gmail.com>
2025-07-21 11:40:00 +0500

    Update reboot.sh

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#
+# ISS AI Hub - System Reboot Script
+#
+set -e
 
+LOG_FILE="/var/log/hub_updates.log"
+echo "--- [$(date)] Reboot command received. Rebooting system in 5 seconds... ---" | tee -a $LOG_FILE
+
+sleep 5
+/sbin/reboot
